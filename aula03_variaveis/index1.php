70e22c8707a5d4ed6d97acf6aafff4f23b560ddb
JeoftonCosta <jeofton_costa@hotmail.com>
2023-09-04 19:29:25 -0300

    Atualizando repositório

---
+++ aula03_variaveis/index1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <form action="processar.php" method="post">
+    <label for="userName">Nome do Usuário</label>
+    <input type="text" name="userName" id="userName">
+    <button type="submit">Save</button>
+  </form>
+</body>
+</html>
